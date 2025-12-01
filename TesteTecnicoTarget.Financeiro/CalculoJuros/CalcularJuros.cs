e897cd97c77206d86e579a776d1918603fbd2fa2
Lucas Gomes Santos <lgsantos306@gmail.com>
2025-12-01 11:04:37 -0300

    Implementa cálculo de juros (funcional e testado, melhorias futuras previstas)

---
+++ TesteTecnicoTarget.Financeiro/CalculoJuros/CalcularJuros.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteTecnicoTarget.Financeiro.CalculoJuros;
+
+internal class CalcularJuros
+{
+    private const decimal TaxaJurosPadrao = 2.5m;
+    public static void Executar(bool usarJurosPadrao = true)
+    {
+        Console.Clear();
+        string titulo = usarJurosPadrao ? "Cálculo de Juros Padrão" : "Cálculo de Juros Personalizados";
+        Console.WriteLine(titulo);
+        for (int i = 0; i < titulo.Length; i++)
+        {
+            Console.Write("-");
+        }
+
+        Console.WriteLine();
+
+        Console.Write("Digite o valor do titulo: ");
+
+        if (!decimal.TryParse(Console.ReadLine(), out decimal valorInicial) || valorInicial < 0)
+        {
+            MensagemErro("Valor inicial inválido. Pressione qualquer tecla para voltar ao menu.");
+            return;
+        }
+
+        Console.Write("Digite a data de vencimento (dd/MM/yyyy): ");
+        if (!DateTime.TryParse(Console.ReadLine(), out DateTime dataVencimento))
+        {
+            MensagemErro("Data de vencimento inválida. Pressione qualquer tecla para voltar ao menu.");
+            return;
+        }
+
+        DateTime dataAtual = DateTime.Now;
+        if (dataAtual <= dataVencimento)
+        {
+            MensagemErro("O título não está vencido. Pressione qualquer tecla para voltar ao menu.");
+            return;
+        }
+        decimal taxaMensal = TaxaJurosPadrao;
+        if (!usarJurosPadrao)
+        {
+            Console.Write("Percentual de juros ao mês (Exemplo 1,5%): ");
+            string entrada = Console.ReadLine()?.Trim();
+
+            // Se tiver ponto, troca por vírgula, tive que fazer isso pois se o usuário digitar com ponto, o parse falha e o 1.0 vira 10%, isso considerando que está tudo fixado para o BR (Idioma e Região)
+            // Preciso mudar para aceitar de qualquer região
+            entrada = entrada.Replace('.', ',');
+
+            if (!decimal.TryParse(entrada, out decimal taxaInformada) || taxaInformada < 0)
+            {
+                MensagemErro("Percentual de juros inválido. Pressione qualquer tecla para voltar ao menu.");
+                return;
+            }
+
+            taxaMensal = taxaInformada;
+        }
+
+        int diasAtraso = (dataAtual - dataVencimento).Days;
+
+        Calcular(diasAtraso: diasAtraso, taxaJurosMensal: taxaMensal, valorInicial: valorInicial);
+    }
+
+    public static void MensagemErro(string msg)
+    {
+        Console.WriteLine($"\n{msg}");
+        Console.ReadKey();
+    }
+
+    public static void Calcular(int diasAtraso, decimal taxaJurosMensal, decimal valorInicial)
+    {
+        decimal taxaDiaria = (taxaJurosMensal / 30m) / 100m;
+        decimal juros = valorInicial * taxaDiaria * diasAtraso;
+        decimal valorTotal = valorInicial + juros;
+
+        Console.WriteLine($"\nValor Inicial: {valorInicial:C}");
+        Console.WriteLine($"Dias em Atraso: {diasAtraso} dias");
+        Console.WriteLine($"Juros Aplicados: {juros:C}");
+        Console.WriteLine($"Valor Total a Pagar: {valorTotal:C}");
+        Console.WriteLine("\nPressione qualquer tecla para voltar ao menu.");
+        Console.ReadKey();
+    }
+}
--- TesteTecnicoTarget.Financeiro/Program.cs
+++ TesteTecnicoTarget.Financeiro/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TesteTecnicoTarget.Financeiro.CalculoJuros;
+using TesteTecnicoTarget.Utilidades;
+
+class Program
+{
+    static void Main()
+    {
+        ExibirMenu();
+    }
+
+    static void ExibirMenu()
+    {
+        while (true)
+        {
+            Console.Clear();
+            MenuHelper.ExibirTitulo("Financeiro");
+
+            Console.WriteLine("1. Calcular Juros padrão");
+            Console.WriteLine("2. Calcular Juros personalizados");
+            Console.WriteLine("0. Sair");
+
+            var opcao = MenuHelper.LerOpcao();
+
+            switch(opcao)
+            {
+                case 0:
+                    return;
+                case 1:
+                    CalcularJuros.Executar();
+                    break;
+                case 2:
+                    CalcularJuros.Executar(false);
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida. Tente novamente.");
+                    break;
+            }
+        }
+
+    }
+
+}
